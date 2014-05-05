a34398f7b9b9576ac90f621c80c3dc6e4cb3af91
Maksim Pecherskiy <max@maksimize.com>
2014-05-05 14:37:13 -0700

    Better cors.

--- server.js
+++ server.js
@@ -68,14 +68,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+// Allow cross-site queries (CORS)
+app.use(cors());
 // Pre Route.
 
-// Allow cross-site queries (CORS)
-app.get('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 app.options('*', function(req, res) {
   res.set({
