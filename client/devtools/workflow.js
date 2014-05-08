459b93164ca4da840b15b7bd873e980deba4c5bc
Maksim Pecherskiy <max@maksimize.com>
2014-05-08 13:11:31 -0700

    Initial.

--- client/devtools/workflow.js
+++
@@ -1,6 +0,0 @@
-/ Adds LiveReload script pointing at the client's hostname.
-// This is helpful for mobile web development where your desktop might point at localhost while
-// your devices point to a local IP address.
-document.write('<script src="http://'
- + window.location.hostname
- + ':35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
