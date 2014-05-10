c4a99909f380ed3db819104ccfc11a7524db1c3b
Maksim Pecherskiy <max@maksimize.com>
2014-05-10 15:19:22 -0700

    Form moving through.

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-	$("#form-3").steps({
+	$("#fundMeWizard").steps({
 	  headerTag: "h3",
 	  bodyTag: "fieldset",
 	  transitionEffect: "slideLeft",
-	  onStepChanging: function (event, currentIndex, newIndex) {
+	  //onStepChanging: function (event, currentIndex, newIndex) {
 	      //$("#form-3").validate().settings.ignore = ":disabled,:hidden";
 	      //return $("#form-3").valid();
-	  },
-	  onStepChanged: function (event, currentIndex, priorIndex) {
+	  //},
+	  /*onStepChanged: function (event, currentIndex, priorIndex) {
 	  },
 	  onFinishing: function (event, currentIndex) {
 	  },
 	  onFinished: function (event, currentIndex){
-	  }
+	  }*/
 	}).validate({});
 });
