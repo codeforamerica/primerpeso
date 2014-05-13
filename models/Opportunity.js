a7373dc689723d83aaa3b9e864a2f69c13bbd641
Maksim Pecherskiy <max@maksimize.com>
2014-05-13 10:16:41 -0700

    Cleaning template.

--- models/Opportunity.js
+++ models/Opportunity.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var _ = require('underscore');
+var admin = require('node-django-admin');
 
 var opSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
@@ -15,6 +16,39 @@ if (process.env.NODE_ENV == 'production') {
   opSchema.set('autoIndex', false);
 }
 
+opSchema.statics.load = function(id, cb) {
+  this.findOne({ _id : id }).exec(cb);
+};
+
+opSchema.statics.list = function(options, cb) {
+  var criteria = options.criteria || {};
+  var order = options.order || {'name': 1};
+
+  this.find(criteria)
+    .sort(order)
+    .limit(options.perPage)
+    .skip(options.perPage * options.page)
+    .exec(cb);
+}
+
+
 var opModel = mongoose.model('Opportunity', opSchema);
+
+/**
+ * Register model in admin
+ */
+
+admin.add({
+  path: 'opportunities',
+  model: 'Opportunity',
+  list: [ 'name' ],
+  edit: [ 'name' ],
+  fields: {
+    'name': {
+       header: 'Name'
+    }
+  }
+});
+
 
 module.exports = opModel;
