3117d60bdb6151ac2482f4d7b3ec00ae2d770c06
Maksim Pecherskiy <max@maksimize.com>
2014-05-13 18:20:40 -0700

    Update model to schema.

--- models/Opportunity.js
+++ models/Opportunity.js
@@ -3,8 +3,41 @@ var _ = require('underscore');
 var admin = require('../custom/fundme-admin');
 
 var opSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
+  purpose: { type: String, required: true },
+  reappliable: { type: Boolean, default: false },
+  // REVIEW
+  eligibleBizLoc: { type: String, required: true },
+  // REVIEW
+  disqualifyingFactors: {type: String, required: true, default: 'none'},
+  paperwork: [{ type: String }],
+  applicationCost: { type: Number, required: true },
+  deadline: { type: Date, required: true },
+  avgApplyTime: { type: Date, required: true },
+  benefitType: [{ type: String, required: true }],
+  benefitDescription: [{ type: String, required: true }],
+  agency: {
+    name: { type: String, required: true },
+    agencyContact: {
+      name: { type: String, required: true },
+      email: { type: String, required: true },
+      phone: { type: String, required: true }
+    }
+  },
+
+  bizEligibility: {
+    minYearsInBiz: { type: String, required: true },
+    eligibleEntityTypes: [{ type: String, required: true }],
+    currentEmp: { type: String, required: true },
+    annualRev: { type: String },
+    eligibleIndustries: [{ type: String, required: true}],
+  },
+
+  audienceEligibility: {
+    gender: { type: String, required: true },
+    age: { type: Number, required: true },
+    additionalDemographics: [{ type: String, required: true}]
+  }
 });
 
 opSchema.pre('save', function(next) {
--- policies/admin.js
+++ policies/admin.js
@@ -3,8 +3,6 @@ var url = require('url');
 module.exports = function(req, res, next) {
   var locals = res.locals;
   var adminPath = req.path;
-  console.log(req.path);
-  console.log('run check');
   if (!req.user) {
     req.flash('errors', { msg: 'You are not authorized to enter Admin.'} );
     return res.redirect('/login');
