68b698490fd3ea6e69e347096e16984dae29edac
Maksim Pecherskiy <max@maksimize.com>
2014-06-17 13:33:40 -0700

    Removing admin css.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,6 +4,5 @@ gulp.task('watch', ['setWatch', 'nodemon', 'browserSync'], function() {
 	//gulp.watch('src/sass/**', ['compass']);
 	gulp.watch('src/images/**', ['images']);
 	gulp.watch('src/htdocs/**', ['copy']);
- // gulp.watch('src/**', ['build']);
 	// Note: The browserify task handles js recompiling with watchify
 });
