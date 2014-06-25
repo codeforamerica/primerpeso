c5d8358ec7276dbe4a729db86684928cb2e606d6
Christian <christian.etpr10@gmail.com>
2014-06-25 10:12:08 -0700

    Add comment for fixing Opportunity Schema

--- models/Opportunity.js
+++ models/Opportunity.js
@@ -20,6 +20,7 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
       }
     },
+    // TODO: Change to choices selection. Add 'other' field
     purpose: {
       type: DataTypes.TEXT,
       allowNull: false,
@@ -28,14 +29,17 @@ module.exports = function(sequelize, DataTypes) {
       },
       label: 'Purpose'
     },
-    canBeReappliedFor: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      widget: 'select',
-      allowNull: false,
-      label: 'Can Be Reapplied For',
-      choices: { 0: 'No', 1:'Yes' }
-    },
+    // TODO: Remove later - Not used
+    // canBeReappliedFor: {
+    //   type: DataTypes.BOOLEAN,
+    //   defaultValue: false,
+    //   widget: 'select',
+    //   allowNull: false,
+    //   label: 'Can Be Reapplied For',
+    //   choices: { 0: 'No', 1:'Yes' }
+    // },
+    // TODO: Add 'other' field and need options for distinct municipalities
+    // TODO: Are we using zipcode's for this? Consider possibility
     eligibleBusinessLocation: {
       type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false,
@@ -44,13 +48,16 @@ module.exports = function(sequelize, DataTypes) {
       choices: choicesList.eligibleBusinessLocation,
       label: 'Eligible Business Location'
     },
-    disqualifyingFactors: {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
-      allowNull: false,
-      multiple: true,
-      widget: 'textArea',
-      label: 'Disqualifying Factors'
-    },
+    // TODO: Remove later - Not used
+    // disqualifyingFactors: {
+    //   type: DataTypes.ARRAY(DataTypes.TEXT),
+    //   allowNull: false,
+    //   multiple: true,
+    //   widget: 'textArea',
+    //   label: 'Disqualifying Factors'
+    // },
+    // Note: These need to be separate inputs on the UI that get joined
+    // in the backend.
     paperworkRequired: {
       type: DataTypes.ARRAY(DataTypes.TEXT),
       widget: 'textArea',
@@ -77,6 +84,7 @@ module.exports = function(sequelize, DataTypes) {
       label: 'Average Application Time'
     },
     // TODO -- abstract choices to freaking callbacks.
+    // TODO: Add 'other' field
     benefitType: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -114,6 +122,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       widget: 'text',
     },
+    // TODO: Change it to options widget. Options are in options.js
     minimumYearsInBusiness: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -144,6 +153,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       label: 'Annual Revenue',
     },
+    // TODO: Need to add eligibleIndustries - other
     eligibleIndustries: {
       type: DataTypes.ARRAY(DataTypes.STRING),
       multiple: true,
@@ -157,8 +167,9 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       label: 'Gender',
       widget: 'radio',
-      choices: { male: 'Male', female: 'Female'}
+      choices: { any: 'Any', male: 'Male', female: 'Female', other: 'Other'}
     },
+    // TODO: Make options field. Options in options.js
     age: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -169,8 +180,11 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       widget: 'checkbox',
       label: 'Additional Demographics',
-      choices: { student: 'student', veteran: 'veteran', minority: 'minority' }
+      choices: { any: 'any', student: 'student', veteran: 'veteran', minority: 'minority' }
     }
+    // TODO: Add additionalGeneralInformation textArea
+    // TODO: Add investingOwnMoney boolean field
+    // TODO: Add moneyInvested text field for whene investingOwnMoney field is true
  }
 
   classMethods = _.extend(modelUtils.classMethods, {});
