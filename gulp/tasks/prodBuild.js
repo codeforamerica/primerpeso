35f98805b4db38562da9b07d5cde22e92d1e7802
Christian <christian.etpr10@gmail.com>
2014-07-02 16:29:32 -0700

    Travis failed. Fixing it.

--- gulp/tasks/prodBuild.js
+++ gulp/tasks/prodBuild.js
@@ -4,4 +4,5 @@ var vendor = require('./vendor');
 var less = require('./less');
 var images = require('./images');
 var copy = require('./copy');
+var migrate = require('./migrate')
 gulp.task('prodBuild', ['browserify', 'vendor', 'less', 'images', 'copy', 'migrate']);
