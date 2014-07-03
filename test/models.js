c751724e4c2c4a84822a510e9ce3507ebcc741cd
Christian <christian.etpr10@gmail.com>
2014-07-03 12:46:49 -0700

    Added first Opportunity model test

--- test/models.js
+++ test/models.js
@@ -1,5 +1,43 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
 var chai = require('chai');
 var should = chai.should();
+var db = require('../models');
+var sequelize = db.sequelize;
+
+describe('Opportunity Model', function() {
+
+  var body = { _csrf: 'undefined', _doc_id: '', title: 'Test opp3', purpose: 'anything', 'purpose-other': '', eligibleBusinessLocation: 'anywhere_in_pr', 'eligibleBusinessLocation-other': '', paperworkRequired: 'asdfasd', applicationCost: '23', applicationDeadline: '2014-12-31', avgApplicationTime: '90 days', benefitType: 'incentive', 'benefitType-other': '', benefitDescription: 'Una descripcion', agencyName: 'Una gencia', agencyContactName: 'Un contacto', agencyContactEmail: 'unemail', agencyContactPhone: '7875663317', minimumYearsInBusiness: '0', eligibleEntityTypes: 'non_profit', currentEmployeesRequired: '26_50', annualRevenue: '0_99999', eligibleIndustries: [ 'any', '' ], gender: 'any', age: '0', additionalGeneralInformation: '', moneyInvested: '' }
+
+  it('should create a new opportunity without optional fields', function(done) {
+    var Opportunity = sequelize.model('opportunity');
+    var instance = Opportunity.buildFromAdminForm(body);
+    instance.validate().
+    success(function(err) {
+      if (err) {
+        done(err);
+      }
+      instance.save().success(function(){
+        done();
+      })
+      .error(function(err) {
+        done(err);
+      });
+    });
+  });
+
+  
+
+  after(function(done) {
+    var Opportunity = sequelize.model('opportunity');
+    Opportunity.destroy({title: 'Test opp3'}).success(function(rows) {
+      done();
+    });
+  });
+});
+
+
 //var User = require('../models/User');
 
 /*describe('User Model', function() {
