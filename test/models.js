c0859170f0e2b6251c633ef987c549d686589584
Maksim Pecherskiy <max@maksimize.com>
2014-07-03 14:45:26 -0700

    Adding beginnings of mock

--- test/models.js
+++ test/models.js
@@ -1,3 +1,4 @@
+
 var dotenv = require('dotenv');
 dotenv.load();
 
@@ -5,11 +6,13 @@ var chai = require('chai');
 var should = chai.should();
 var db = require('../models');
 var sequelize = db.sequelize;
+var oppMock = require('./mocks/opportunity.js');
 
 describe('Opportunity Model', function() {
 
   it('should create a new opportunity without optional fields', function(done) {
-    var body = { _csrf: 'undefined', _doc_id: '', title: 'Test opp3', purpose: 'anything', 'purpose-other': '', eligibleBusinessLocation: 'anywhere_in_pr', 'eligibleBusinessLocation-other': '', paperworkRequired: 'asdfasd', applicationCost: '23', applicationDeadline: '2014-12-31', avgApplicationTime: '90 days', benefitType: 'incentive', 'benefitType-other': '', benefitDescription: 'Una descripcion', agencyName: 'Una gencia', agencyContactName: 'Un contacto', agencyContactEmail: 'unemail', agencyContactPhone: '7875663317', minimumYearsInBusiness: '0', eligibleEntityTypes: 'non_profit', currentEmployeesRequired: '26_50', annualRevenue: '0_99999', eligibleIndustries: [ 'any', '' ], gender: 'any', age: '0', additionalGeneralInformation: '', moneyInvested: '' }
+    var mock = oppMock({
+    });
     var Opportunity = sequelize.model('opportunity');
     var instance = Opportunity.buildFromAdminForm(body);
     instance.validate().
@@ -56,12 +59,12 @@ describe('Opportunity Model', function() {
         should.exist(err);
         done();
       }
-    }); 
+    });
   });
 
   it('should correctly parse and save "other" text fields', function(done) {
     // purpose other is filled out, additionalGeneralInformation is filled out
-    var body = { _csrf: 'undefined', _doc_id: '', title: 'Test opp3', purpose: 'other', 'purpose-other': 'A different purpose', eligibleBusinessLocation: 'anywhere_in_pr', 'eligibleBusinessLocation-other': '', paperworkRequired: 'asdfasd', applicationCost: '23', applicationDeadline: '2014-12-31', avgApplicationTime: '90 days', benefitType: 'incentive', 'benefitType-other': '', benefitDescription: 'Una descripcion', agencyName: 'Una gencia', agencyContactName: 'Un contacto', agencyContactEmail: 'unemail', agencyContactPhone: '7875663317', minimumYearsInBusiness: '0', eligibleEntityTypes: 'non_profit', currentEmployeesRequired: '26_50', annualRevenue: '0_99999', eligibleIndustries: [ 'any', ], gender: 'any', age: '0', additionalGeneralInformation: 'Some other general information', moneyInvested: '' }
+    var body = { _csrf: 'undefined', _doc_id: '', title: 'Test opp3', purpose: 'other', 'purpose-other': 'A different purpose', eligibleBusinessLocation: 'anywhere_in_pr', 'eligibleBusinessLocation-other': '', paperworkRequired: 'asdfasd', applicationCost: '23', applicationDeadline: '2014-12-31', avgApplicationTime: '90 days', benefitType: 'incentive', 'benefitType-other': '', benefitDescription: 'Una descripcion', agencyName: 'Una gencia', agencyContactName: 'Un contacto', agencyContactEmail: 'unemail', agencyContactPhone: '7875663318', minimumYearsInBusiness: '0', eligibleEntityTypes: 'non_profit', currentEmployeesRequired: '26_50', annualRevenue: '0_99999', eligibleIndustries: [ 'any', ], gender: 'any', age: '0', additionalGeneralInformation: 'Some other general information', moneyInvested: '' }
     var Opportunity = sequelize.model('opportunity');
     var instance = Opportunity.buildFromAdminForm(body);
     instance.validate().
