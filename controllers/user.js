42d2d5a9b6ed9ebd6e00dd14623c548331bb03a4
Christian <christian.etpr10@gmail.com>
2014-07-07 15:09:51 -0700

    Changed login redirect. It used to redirect me to favicon.ico

--- controllers/user.js
+++ controllers/user.js
@@ -69,7 +69,8 @@ var postLogin = function(req, res, next) {
     req.logIn(user, function(err) {
       if (err) return next(err);
       req.flash('success', { msg: 'Success! You are logged in.' });
-      res.redirect(req.session.returnTo || '/');
+      res.redirect('/');
+      // res.redirect(req.session.returnTo || '/');
     });
   })(req, res, next);
 };
