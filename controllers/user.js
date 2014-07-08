ab971ec19dea821f5c64d9ad00956e5e7ce42143
Christian <christian.etpr10@gmail.com>
2014-07-07 17:17:09 -0700

    Add TODO comment

--- controllers/user.js
+++ controllers/user.js
@@ -69,8 +69,8 @@ var postLogin = function(req, res, next) {
     req.logIn(user, function(err) {
       if (err) return next(err);
       req.flash('success', { msg: 'Success! You are logged in.' });
+      // TODO: User should be redirected to previous page on login.
       res.redirect('/');
-      // res.redirect(req.session.returnTo || '/');
     });
   })(req, res, next);
 };
