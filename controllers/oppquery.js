d46513e7574710b7c5464dadb789eac54fa87df7
Maksim Pecherskiy <max@maksimize.com>
2014-07-09 16:01:09 -0700

    Adding basic templating for res page.

--- controllers/oppquery.js
+++ controllers/oppquery.js
@@ -26,10 +26,64 @@ var oppQueryCreateJson = function(req, res, next) {
 }
 
 var oppQueryExecute = function(req, res, next) {
+  var searchResult = {
+    loans: {
+      equipmentFinancing: {
+        id: 1,
+        title: 'Test Opportunity',
+        estimatedTime: '30 dias',
+        deadline: '2014-12-30 16:00:00-08',
+        description: 'some description',
+        name: 'equipmentFinancing'
+      },
+      mechanicHelp: {
+        id: 2,
+        title: 'Test Opportunity2',
+        estimatedTime: '30 dias',
+        deadline: '2014-12-30 16:00:00-08',
+        description: 'some description',
+        name: 'mechanicHelp'
+      },
+      phoneBorrowing: {
+        id: 3,
+        title: 'Test Opportunity3',
+        estimatedTime: '30 dias',
+        deadline: '2014-12-30 16:00:00-08',
+        description: 'some description',
+        name: 'phoneBorrowing'
+      }
+    },
+    grants: {
+      funGrant: {
+        id: 4,
+        title: 'Test Opportunity',
+        estimatedTime: '30 dias',
+        deadline: '2014-12-30 16:00:00-08',
+        description: 'some description',
+        name: 'funGrant'
+      },
+      moneyFromTheGovernment: {
+        id: 5,
+        title: 'Test Opportunity2',
+        estimatedTime: '30 dias',
+        deadline: '2014-12-30 16:00:00-08',
+        description: 'some description',
+        name: 'moneyFromTheGovernment'
+      },
+      somethingElseIrrelevant: {
+        id: 6,
+        title: 'Test Opportunity3',
+        estimatedTime: '30 dias',
+        deadline: '2014-12-30 16:00:00-08',
+        description: 'some description',
+        name: 'somethingElseIrrelevant'
+      }
+    }
+  };
   res.render('searchResults', {
     title: 'Search Results',
     bodyClass: 'searchResults',
     isSearch: true,
-    searchResult: {}
+    searchResult: searchResult
   });
 };
