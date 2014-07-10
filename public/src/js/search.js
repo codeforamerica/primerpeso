9b2fb5f3e7c9691f47963ac24677887373ed2c6a
Maksim Pecherskiy <max@maksimize.com>
2014-07-10 12:09:32 -0700

    Implementing search view logic.

--- public/src/js/search.js
+++ public/src/js/search.js
@@ -7,15 +7,32 @@ Backbone.$ = $;
 module.exports = Backbone.View.extend({
   el: $('body'), // attaches `this.el` to an existing element.
   events: {
-    'click button#add': 'addItem'
+    'click li.search-tab a': 'switchTab'
   },
 
   initialize: function(){
-    _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+    _.bindAll(this, 'render', 'switchTab'); // fixes loss of context for 'this' within methods
     this.render(); // not all views are self-rendering. This one is.
   },
 
-  render: function(){
-  //  $(this.el).append("<ul> <li>hello world</li> </ul>");
+  render: function(){},
+
+  switchTab: function(e) {
+    var targetId = $(e.currentTarget).attr('id');
+    $('.search-tabs .active').removeClass('active');
+    $(e.currentTarget).parent().addClass('active');
+    if (targetId == 'all') {
+      $('.opportunities-container').children().each(function(index, element){
+        $(element).show();
+      });
+    }
+    else {
+      $('.opportunities-container').children().each(function(index, element){
+        $(element).hide()
+      });
+      $('.' + targetId + '-container').show();
+    }
   }
+
+
 });
