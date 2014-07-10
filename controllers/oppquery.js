bb3746e9e4b67de984d7163e4e77536782b96c09
Maksim Pecherskiy <max@maksimize.com>
2014-07-10 12:14:40 -0700

    Moving mock to a mock file.

--- controllers/oppquery.js
+++ controllers/oppquery.js
@@ -1,4 +1,5 @@
 var oppQueryForm = require('../lib/oppQueryForm.js');
+var searchResults = require('../test/mocks/searchResults');
 
 module.exports = function(app) {
   app.get('/fundme', oppQueryCreate);
@@ -11,7 +12,6 @@ module.exports = function(app) {
  */
 var oppQueryCreate = function(req, res, next) {
   var options = options || {};
-//  return res.json(oppQueryForm.getFormConfig());
   res.render('fundmeWizard', {
     title: 'Wizard',
     bodyClass: 'fundmeWizard',
@@ -26,60 +26,7 @@ var oppQueryCreateJson = function(req, res, next) {
 }
 
 var oppQueryExecute = function(req, res, next) {
-  var searchResult = {
-    loans: {
-      equipmentFinancing: {
-        id: 1,
-        title: 'Test Opportunity',
-        estimatedTime: '30 dias',
-        deadline: '2014-12-30 16:00:00-08',
-        description: 'some description',
-        name: 'equipmentFinancing'
-      },
-      mechanicHelp: {
-        id: 2,
-        title: 'Test Opportunity2',
-        estimatedTime: '30 dias',
-        deadline: '2014-12-30 16:00:00-08',
-        description: 'some description',
-        name: 'mechanicHelp'
-      },
-      phoneBorrowing: {
-        id: 3,
-        title: 'Test Opportunity3',
-        estimatedTime: '30 dias',
-        deadline: '2014-12-30 16:00:00-08',
-        description: 'some description',
-        name: 'phoneBorrowing'
-      }
-    },
-    grants: {
-      funGrant: {
-        id: 4,
-        title: 'Test Opportunity',
-        estimatedTime: '30 dias',
-        deadline: '2014-12-30 16:00:00-08',
-        description: 'some description',
-        name: 'funGrant'
-      },
-      moneyFromTheGovernment: {
-        id: 5,
-        title: 'Test Opportunity2',
-        estimatedTime: '30 dias',
-        deadline: '2014-12-30 16:00:00-08',
-        description: 'some description',
-        name: 'moneyFromTheGovernment'
-      },
-      somethingElseIrrelevant: {
-        id: 6,
-        title: 'Test Opportunity3',
-        estimatedTime: '30 dias',
-        deadline: '2014-12-30 16:00:00-08',
-        description: 'some description',
-        name: 'somethingElseIrrelevant'
-      }
-    }
-  };
+  var searchResult = searchResults();
   res.render('searchResults', {
     title: 'Search Results',
     bodyClass: 'searchResults',
