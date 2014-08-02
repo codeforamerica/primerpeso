bab9957c8c18652bf0ae486cdf514759f0d1c9ad
Maksim Pecherskiy <max@maksimize.com>
2014-08-02 12:09:24 -0400

    Beginning work on mandrill.

--- config/secrets.js
+++ config/secrets.js
@@ -6,18 +6,7 @@ module.exports = {
   sessionSecret: process.env.SESSION_SECRET || 'I Am A Monkey',
   staticFilePrefix: process.env.STATIC_FILE_PREFIX || '',
 
-  mailgun: {
-    login: process.env.MAILGUN_LOGIN || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
-    password: process.env.MAILGUN_PASSWORD || '29eldds1uri6'
-  },
-
   mandrill: {
-    login: process.env.MANDRILL_LOGIN || 'hackathonstarterdemo',
-    password: process.env.MANDRILL_PASSWORD || 'E1K950_ydLR4mHw12a0ldA'
-  },
-
-  sendgrid: {
-    user: process.env.SENDGRID_USER || 'hslogin',
-    password: process.env.SENDGRID_PASSWORD || 'hspassword00'
-  },
+    apiKey: process.env.MANDRILL_API_KEY || '',
+  }
 };
