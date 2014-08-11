25339395d7f8fc85f5abf20f6f5c99951f1945ad
Maksim Pecherskiy <max@maksimize.com>
2014-08-11 18:23:54 -0400

    Successfully creating agencies via migration.

--- migrations/20140811145141-separate-agency.js
+++ migrations/20140811145141-separate-agency.js
@@ -7,17 +7,33 @@ module.exports = {
     var sequelize = migration.migrator.sequelize;
     // add altering commands here, calling 'done' when finished
     var Opportunity = sequelize.model('opportunity');
+    var User = migration.migrator.sequelize.model('user');
+    var Agency = sequelize.model('agency');
+    var defaultCreator;
 
     migration.addColumn( 'opportunities', 'agencyId', {
       type: DataTypes.INTEGER
     }).then(function() {
+      return User.find({ where: { email: 'clara@codeforamerica.org' } });
+    }).then(function(user) {
+      // Set user.
+      defaultCreator = user;
       // Get a "pivot table" of all the agencies in opportunities ATM.
-      var sql = 'SELECT DISTINCT "agencyName" FROM "opportunities"',
+      var sql = 'SELECT DISTINCT "agencyName" FROM "opportunities"';
       return sequelize.query(sql,  Opportunity, { raw: true });
     }).then(function(agencies) {
       console.log(agencies);
+      var promiseContainer = [];
+      _.each(agencies, function(agencyObject, index) {
+        promiseContainer.push(Agency.create({
+          name: agencyObject.agencyName,
+          creatorId: defaultCreator.id
+        }));
+      });
+      return Promise.all(promiseContainer);
+    }).then(function() {
+      return done();
     });
-    done();
 },
   down: function(migration, DataTypes, done) {
     // add reverting commands here, calling 'done' when finished
