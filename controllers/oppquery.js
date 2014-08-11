a9c145be15728a8cfcb7c2e32b7fd26a08ed5c2e
Maksim Pecherskiy <max@maksimize.com>
2014-08-11 11:29:38 -0400

    Change email subject to primerpeso

--- controllers/oppquery.js
+++ controllers/oppquery.js
@@ -91,7 +91,7 @@ var oppQuerySendLead = function(req, res, next) {
   var leadData = req.body;
   leadData.cartContents = req.session.cartContents || {};
   mailBoss.send({
-    subject: "Primer Peso Lead Form Submission",
+    subject: "PrimerPeso Lead Form Submission",
     text: JSON.stringify(leadData, null, 4)
   }, function(err, info) {
       console.log(err);
