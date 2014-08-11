6d0836f82d8f7ea4cd663c42b20ad283ca10568d
Maksim Pecherskiy <max@maksimize.com>
2014-08-11 14:42:43 -0400

    Add initial file for Agency model.

---
+++ models/defs/Agency.js
@@ -0,0 +1,48 @@
+var modelUtils = require('../lib/modelUtils.js');
+var _ = require('lodash');
+var OptionsList = require('../../lib/OptionsList');
+
+module.exports = function(sequelize, DataTypes) {
+  var attributes = {};
+  var choicesList = new OptionsList();
+  var classMethods = {};
+  var instanceMethods = {};
+  attributes = {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true
+    },
+    name:  {
+      type: DataTypes.STRING,
+      allowNull: false,
+      label:'Agency Name',
+      unique: true,
+      validate: {
+      }
+    },
+    // Association
+    creatorId: {
+      type: DataTypes.INTEGER,
+    }
+  }
+
+  classMethods = _.extend(modelUtils.classMethods, {
+    getListFields: function() {
+      return {
+        'name': 'Agency Name',
+      };
+    },
+    associate: function(sequelize) {
+      var User = sequelize.model('user');
+      this.belongsTo(User, { as: 'creator', foreignKey: 'creatorId' });
+    }
+  });
+  instanceMethods = _.extend(modelUtils.instanceMethods, {});
+
+  return sequelize.define('agency', attributes, {
+    classMethods: classMethods,
+    instanceMethods: instanceMethods
+  });
+}
