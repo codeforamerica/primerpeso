393001948327322d2c9378a1c7b8ab05ffa2ea96
Maksim Pecherskiy <max@maksimize.com>
2014-08-11 22:53:23 -0400

    Add a space to prodbuild task.

--- gulp/tasks/prodBuild.js
+++ gulp/tasks/prodBuild.js
@@ -5,4 +5,5 @@ var less = require('./less');
 var images = require('./images');
 var copy = require('./copy');
 var migrate = require('./migrate')
+
 gulp.task('prodBuild', ['browserify', 'vendor', 'less', 'images', 'copy', 'migrate']);
