076894301e738f7ae22dc446df9dab6bb02713b3
Maksim Pecherskiy <max@maksimize.com>
2014-09-02 17:54:47 -0700

    Adding import of csv of agencies.

---
+++ migrations/20140902161922-import-agencies.js
@@ -0,0 +1,42 @@
+var _ = require('lodash');
+var Sequelize = require('sequelize');
+var Promise = Sequelize.Promise;
+var CSV = require('comma-separated-values');
+var fs = require('fs');
+Promise.promisifyAll(fs);
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    var sequelize = migration.migrator.sequelize;
+    // add altering commands here, calling 'done' when finished
+    var Opportunity = sequelize.model('opportunity');
+    var User = sequelize.model('user');
+    var Agency = sequelize.model('agency');
+    var defaultCreator;
+
+    User.find({ where: { email: 'clara@codeforamerica.org' } }).then(function(user) {
+      // Set user.
+      defaultCreator = user;
+      return fs.readFileAsync(__dirname + '/../data_import/agencies.csv', { encoding: 'utf8' });
+    }).then(function(data) {
+      var parsedData = new CSV(data, {
+        header: ['name', 'mission', 'address', 'municipality', 'zip', 'phone', 'fax', 'web']
+      }).parse();
+      parsedData = _.map(parsedData, function(agencyData) {
+        return _.extend(agencyData, { creatorId: defaultCreator.id });
+      });
+      return Agency.bulkCreate(parsedData);
+    }).success(function() {
+      return done();
+    }).error(function(err) {
+      return done(err)
+    });
+
+
+    //done();
+  },
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    done()
+  }
+}
